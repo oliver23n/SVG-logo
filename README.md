143ef616875e0579983d887574c225ea86e6e386
oliver23n <oliver.nasevski@gmail.com>
2023-06-07 17:24:35 -0400

    added README

---
+++ README.md
@@ -0,0 +1,76 @@
+# SVG logo generator
+
+[![alt badge](https://img.shields.io/badge/licence-MIT-blue)](https://opensource.org/license/mit/)
+
+## Description
+
+Based on the users input in the CLI, this app creates a LOGO which is a .svg file.
+
+---
+
+## Table Of Contents
+                             
+- [Description](#description)
+- [Installation](#installation)
+- [Usage](#usage)
+- [License](#license)
+- [Contributing](#contributing)
+- [Tests](#tests)
+- [Questions](#questions)
+
+---
+
+## Installation
+
+To install the required dependencies, just run this code: 
+
+```
+npm i
+```
+
+---
+
+## Usage
+
+To start the application must run the following code in CLI
+
+```
+node index.js
+```
+Here is a video link for a demonstration on how to use it:
+[https://drive.google.com/file/d/1BUtKXzb8LaEP2jEvgTU71ULpwSxyQ9HV/view](https://drive.google.com/file/d/1BUtKXzb8LaEP2jEvgTU71ULpwSxyQ9HV/view)
+
+
+---
+
+## License
+
+This Project is licenced under MIT license.
+
+---
+
+## Contributing
+
+N/A
+
+---
+
+## Tests
+
+To run tests, just run the following command:
+
+```
+npm run test
+```
+
+--- 
+
+
+## Questions
+
+You can find more about my work at my Github [oliver23n](https://github.com/oliver23n).
+
+
+
+
+
--- index.js
+++ index.js
@@ -1,25 +1,6 @@
-// I WANT to generate a simple logo for my projects
-// GIVEN a command - line application that accepts user input
-// WHEN I am prompted for text
-// THEN I can enter up to three characters
-// WHEN I am prompted for the text color
-// THEN I can enter a color keyword(OR a hexadecimal number)
-// WHEN I am prompted for a shape
-// THEN I am presented with a list of shapes to choose from: circle, triangle, and square
-// WHEN I am prompted for the shape's color
-// THEN I can enter a color keyword(OR a hexadecimal number)
-// WHEN I have entered input for all the prompts
-// THEN an SVG file is created named`logo.svg`
-// AND the output text "Generated logo.svg" is printed in the command line
-// WHEN I open the `logo.svg` file in a browser
-// THEN I am shown a 300x200 pixel image that matches the criteria I entered
-
 const inquirer = require('inquirer');
 const fs = require('fs');
-const Triangle = require('./lib/Triangle');
-const Circle = require('./lib/Circle');
-const Square = require('./lib/Square');
-
+const createShape = require('./lib/createShape');
 
 
 const questions = [
@@ -46,47 +27,21 @@ const questions = [
     }
 ];
 
-function createShape({text,colorText,colorShape,shape}){
-    let logo;
-    text = text.toUpperCase();
-    //check what type of shape user inputs
-    switch (shape){
-        //make a class with it 
-        case 'triangle':
-            logo = new Triangle();
-            break;
-        case 'circle':
-            logo = new Circle();
-            break;
-        case 'square':
-            logo = new Square();
-            break;
-    }
-    console.log(shape);
-
-    //set the color of the shape
-    logo.setColor(colorShape);
-    return `<svg version="1.1" width="300" height="200" xmlns="http://www.w3.org/2000/svg">
-
-  ${logo.render()}
-
-  <text x="150" y="125" font-size="60" text-anchor="middle" fill="${colorText}">${text}</text>
-
-</svg>
-    `
-}
-
-function writeToFile( name, data){
+function writeToFile(name, data) {
 
     fs.writeFile(`./examples/${name}.svg`, data, err => err ? console.error(err) : console.log('Generated logo.svg!'));
-    
+
 }
 inquirer.prompt(questions)
-.then((response) => {
-    //get the shape, color , text variables
-    //pass into create and get str return
-    const dataToWrite = createShape(response);
-    writeToFile('logo',dataToWrite);
-})
+    .then((response) => {
+        if(response.text.length > 3 || response.text.length == 0){
+            throw new Error('Text must be less than 3 chars')
+        }else{
+            //get the shape, color , text variables
+            //pass into create and get str return
+            const dataToWrite = createShape(response);
+            writeToFile('logo', dataToWrite);
+        }
+    })
 
 
